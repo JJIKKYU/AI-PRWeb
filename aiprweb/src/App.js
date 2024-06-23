b8710a16ec67d3d6511695d2dcd5cdf5823338ad
JJIKKYU <jjinggu96@gmail.com>
2024-06-23 21:47:07 +0900

    add firebase

--- aiprweb/src/App.js
+++ aiprweb/src/App.js
@@ -1,4 +1,27 @@
 import logo from './logo.svg';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCIDlYyy0i67UMxOM6W1A_5K18IoVIAt14",
+  authDomain: "ai-prweb.firebaseapp.com",
+  projectId: "ai-prweb",
+  storageBucket: "ai-prweb.appspot.com",
+  messagingSenderId: "143086048191",
+  appId: "1:143086048191:web:e8dac4df3466890eec470a",
+  measurementId: "G-GXW21K7R6E"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 import './App.css';
 
 function App() {
